0870603b23f1f8ad8218143afa571ce2a95ba736
avi-hostfi <105990027+avi-hostfi@users.noreply.github.com>
2025-04-02 21:03:32 -0700

    add clerk manager to manager list

--- app/managers/__init__.py
+++ app/managers/__init__.py
@@ -1,9 +1,11 @@
 from app.managers.base_manager import BaseManager
+from app.managers.clerk_manager import ClerkManager
 from app.managers.organization_manager import OrganizationManager
 from app.managers.user_manager import UserManager
 
 __all__ = [
     "BaseManager",
-    "UserManager",
+    "ClerkManager",
     "OrganizationManager",
+    "UserManager",
 ]
